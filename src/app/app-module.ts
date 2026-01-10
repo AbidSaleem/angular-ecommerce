18f3df4f24a46bd29dad2d37a7cfa513d828c99c
Abid <abidsaleem@live.com>
2026-01-10 13:56:42 +0300

    Fetch product list and show it in a table

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,17 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ProductList } from './components/product-list/product-list';
+import { provideHttpClient } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    App
+    App,
+    ProductList
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient()
   ],
   bootstrap: [App]
 })
